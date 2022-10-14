16660a7d669461e9c40ee68e9ced89ffa0c6f722
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-13 19:00:29 -0700

    3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,2 @@
-#include<studio.h>
-/** 
- * main-Serves as the entry point for the program *
- 
- * Return: By default returns zero if no erros otherwise returns non-zero value.
- */
-int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+#include <studio.h>
+int main(void){ puts("\"Programming is like building a multilingual puzzle\n");return (0);}	
