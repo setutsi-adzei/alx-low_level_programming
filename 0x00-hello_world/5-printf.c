487b5f1a119da174f98d020968bf0cc9611fcad9
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-13 19:19:34 -0700

    4

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <studio.h>
+/**
+ *   main - prints the phrase with proper grammar,
+ *   but the outcome is a piece of art,
+ *   Return: 0 after printing the function
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0); }
