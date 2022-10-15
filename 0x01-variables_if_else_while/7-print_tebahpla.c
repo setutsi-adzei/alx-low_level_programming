c1cfaf6ac7e1e40ed6549e2eb72424bf3084d534
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-14 18:48:46 -0700

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Returns zero 0 (succes)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
