e672a6d2ee2b3284ce9b1463763d810023ade911
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-14 18:08:51 -0700

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Returns zero - (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'd';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'Z') {
+			putchar(c);
+			d++;
+		}
+	putchar('\n');
+	return (0);
+}
