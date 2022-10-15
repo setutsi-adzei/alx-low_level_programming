0297316b42c9fcb05b75cc578a2f816a155cc27b
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-14 19:11:16 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ *
+ * Return: Returns zero 0 (success)
+ */
+int main(void)
+	{
+		int a;
+	
+		for (a < 0; a < 10; a++)
+		{
+			putchar(a + '0');
+			if (a < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
