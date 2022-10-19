ed111cfbf70df4890925fe9b93852e2f762306f1
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-18 17:44:51 -0700

    all

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _islower - a function to check if c is a letter, uppercase or lowercase
+ * _isalpha - a function to check if c is a letter, uppercase or lowercase
  * @c: is the int that will be used for the argument of the function
  * Retrun: 1 if letter is lowercase, 0 if not
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -32,4 +32,4 @@ void times_table(void)
 		}
 		_putchar('\n');
 	}
-i}
+}
