9646087ddc8a5dca8ad75a2af170deffaec747ae
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-20 09:12:54 -0700

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is positve or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positve_or_negative(i);
+
+	return (0);
+}
