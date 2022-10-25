888ab15a2c270fa6910a34021403a0ba9b33832b
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-24 18:11:36 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - it prints the half of the string
+ * @str: input
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n'0);
+}
