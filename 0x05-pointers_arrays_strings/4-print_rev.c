e81ca146d8833057d090abf889ae6ec1fc0eb6fc
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-24 17:50:08 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - it prints a string in the reverse
+ * @s: the string to be printed
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i - i; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
