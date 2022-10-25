beee8e184e7e88309bc1ff86bb1430d8068b299b
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-24 17:37:07 -0700

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two numbers
+ * @a: int a
+ * @b: int b
+ */
+void swap_int(int *a, int *b)
+{
+	int q;
+
+	q = *a;
+	*a = *b;
+	*b = q;
+}
