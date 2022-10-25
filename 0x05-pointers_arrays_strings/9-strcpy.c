05903edffeb46c690bc1a4575c10cc1cf30e10dd
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-10-24 18:23:45 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: the destination
+ * @src: the source
+ * Return: the string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0;
+
+	while (*(src + len) != '\0')
+	{
+		*(dest + len) = *(src + len);
+		len++
+	}
+	*(dest + len) = '\0';
+	return (dest);
+}
