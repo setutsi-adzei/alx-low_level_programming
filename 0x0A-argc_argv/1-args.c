aee606cb3bd1d5a754031baef3e975e1aaa7a95d
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-11-04 18:30:52 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: array of a commanf listed
  * Return: 0 (success)
  */
-int main(int argc,char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
