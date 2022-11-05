4bb3bdb7328ceace363bc75d81b7f5e6a5d6106b
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-11-04 18:15:32 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - the function to print name
+ * @argc: argc parameter
+ * @argv: array of a command listed
+ * Return: 0 (success)
+ */
+int main(int argc _attribute_((unused)), char *argv[])
+{
+	printf("%s\n", *argv)
+		return (0);
+}
---
+++ 0x0A-argc_argv/_puchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the char
+ * @c: char
+ * Return: 0 (success)
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
