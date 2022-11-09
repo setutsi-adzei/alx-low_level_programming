824474fc17276c3e27f2d6d07c284b0f8298cf56
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-11-08 19:45:09 -0800

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,12 +10,13 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *array;
+
 	if (size == 0)
-		return(NULL);
+		return (NULL);
 	array = malloc(size * sizeof(char));
 	if (array == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 	for (i = 0; i < size; i++)
 	{
