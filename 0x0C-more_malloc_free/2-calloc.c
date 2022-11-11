6198bd55c1e1fea81656e6fb47715bc11bdc9ef2
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-11-11 07:33:14 -0800

    2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,6 +15,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
+	mem = malloc(size * nmemb);
+
+	if (mem == NULL)
+		return (NULL);
+
 	filler = mem;
 
 	for (index = 0; index < (size * nmemb); index++)
