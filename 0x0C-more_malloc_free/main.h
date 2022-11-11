16ec7a9e0e91e0f928c9a0a9d50f7398595727bd
Adzei Setutsi Yao <adzeisetutsi8@gmail.com>
2022-11-11 08:14:32 -0800

    4

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,6 @@ void *malloc_checked(unsigned int b);
 int _putchar(char c);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int *array_range(int min, int max);
 #endif
